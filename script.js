e6ac8c1396b9069cc326357d0930dd99b81b653e
irakli-dadu <lipsowow@gmail.com>
2022-07-28 02:50:18 +0400

    done
    ?!

---
+++ script.js
@@ -0,0 +1,157 @@
+// let user = {
+//     firstname: 'giorgi',
+//     lastname: 'smith',
+//     age: 25,
+//     studentstatus: 'active'
+// }
+// console.log(user.studentstatus);
+
+
+// -----------------------------------------------2-----------------------------------------
+
+// let elements = ['irakli', 29, 'giorgi', 'mindia', 'kacurikaci', 'tryhardera']
+
+// for (let i of elements)
+// console.log(i);
+
+
+
+
+// -----------------------------------------------3-----------------------------------
+
+// let numbers = [5, 6, 10, 25, 3, 7, 4, 100, 28, 36, 89]
+
+// for (let i = 0; i < numbers.length; i++) {
+//     if (numbers[i] > 5) {
+//         console.log(numbers[i]);
+//     }
+// }
+
+
+
+
+
+// ------------------------------------------------4-------------------------------------------
+
+
+// let user1 = {
+//     name: 'giorgi',
+//     age: 20,
+//     studentstatus: 'active'
+// }
+// if (user1.age < 18 && user1.studentstatus == 'active') {
+//     // console.log('hello');
+// }
+// else if (user1.name = 'levani') {
+//     // console.log('hello giorgi');
+// }
+// else if (user1.studentstatus == 'active' || user1.age < 25) {
+//     // console.log('hello world');
+// }
+// else {
+//     console.log('error');
+// }
+
+
+
+
+
+
+// ------------------------------------------5---------------------
+
+// let items = [ 'watermelon', 'pear', 10, 45, 50, 'apple', 'ananas' ]
+
+// for (textz of items) {
+//     if (typeof textz == 'string') {
+//         console.log(textz);
+//     }
+// }
+
+
+
+
+
+
+// -------------------------------------6------------------------------
+
+// let numeros = [32, 10, 'hello', null, 'hello2', 50 ]
+
+// for (let y of numeros) {
+//     if (y % 5 == 0 && y != null) {
+//         console.log(y);
+//     }
+// }
+
+
+
+
+
+
+// ----------------------------------7------------------------------
+
+// let massive = [ [2, -3, 5, 10], [25, -24, -11, 100] , [-6, -7, 10] ];
+
+// for (let x1 = 0; x1 < massive.length; x1++) {
+
+//       for (let i1 = 0; i1 < massive[x1].length; i1++) {
+//         if (massive[x1][i1] > 0) {
+//             console.log(massive[x1][i1]);
+//         }
+//     }
+// }
+
+
+
+// -------------------------8-------------------------------------------------
+
+// let array = [ 2, 3, 5, 10,25,24,11, 100 , 6,7, 10 ]
+
+// for (let x of array) {
+//     if (x % 2 == 0) {
+//         console.log(x);
+//     }
+// }
+
+// for (let y of array) {
+//     if (y % 2 == 1) {
+//         console.log(y);
+//     }
+// }
+
+
+
+
+
+// ------------------------------------------9-------------------------------------------
+
+
+
+// let languages = ['ka', 'en']
+// let y = 'gaumarjos'
+// let eng = 'englando fernando'
+
+// for (let x of languages) {
+//     if (x == 'ka') {
+//         // console.log(y);
+//     }
+//     if (x == 'en') {
+//         console.log(eng);
+//     }
+// }
+
+
+
+// switch case-it ragacas ver vaketeb sworad...
+
+// let free = ['ka', 'en']
+// let y = 'gaumarjos'
+// let eng = 'englando fernando'
+
+// switch(free) {
+//     case 'ka':
+//         console.log(y);
+//     break;
+// }
+
+
+
